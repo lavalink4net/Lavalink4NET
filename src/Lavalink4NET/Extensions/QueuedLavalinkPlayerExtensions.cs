ab05f902af32f9547e423316e1fcb52a05346a1e
NycroV <83246959+NycroV@users.noreply.github.com>
2024-09-22 23:43:05 -0400

    Documentation

--- src/Lavalink4NET/Extensions/QueuedLavalinkPlayerExtensions.cs
+++ src/Lavalink4NET/Extensions/QueuedLavalinkPlayerExtensions.cs
@@ -8,6 +8,15 @@ using Lavalink4NET.Rest.Entities.Tracks;
 
 public static class QueuedLavalinkPlayerExtensions
 {
+    /// <summary>
+    /// Adds a <see cref="TrackLoadResult"/> to the player's queue.<br/>
+    /// Returns the index of the track in the queue (or the last track added to the queue if <paramref name="loadResult"/> is a playlist)
+    /// </summary>
+    /// <param name="player">The player to enqueue the load result to.</param>
+    /// <param name="loadResult">The load result you want to enqueue.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The index of the track in the queue (or the last track added to the queue if <paramref name="loadResult"/> is a playlist)</returns>
+    /// <exception cref="InvalidOperationException">When the <paramref name="loadResult"/> contains no tracks.</exception>
     public static async ValueTask<int> PlayAsync(this IQueuedLavalinkPlayer player, TrackLoadResult loadResult, CancellationToken cancellationToken = default)
     {
         cancellationToken.ThrowIfCancellationRequested();
